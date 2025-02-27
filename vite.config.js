117f31a4782fe9be13d34f6668f8f4dfa05921a3
abdelrahman-tamim <boditamim2013@gmail.com>
2025-02-27 17:05:36 +0200

    any

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
+});
